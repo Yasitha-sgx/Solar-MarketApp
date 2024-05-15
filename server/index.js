abbf2fe0ef7bd0fa55d3e568b8f784385564b2e0
YasithaRDK <yasithadilshanrajapaksha@gmail.com>
2024-05-15 11:46:51 +0530

    changes for deploy

--- server/index.js
+++ server/index.js
@@ -29,10 +29,10 @@ app.use("/api/users", userRouter);
 app.use("/api/quotations", quotationRouter);
 app.use("/api/offers", offerRouter);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 connectDB()
