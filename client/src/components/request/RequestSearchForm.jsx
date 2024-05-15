d8abad071b62625484392df2cc6b9ed38c92cf91
YasithaRDK <yasithadilshanrajapaksha@gmail.com>
2024-05-15 15:42:24 +0530

    complete request search form UI

--- client/src/components/request/RequestSearchForm.jsx
+++ client/src/components/request/RequestSearchForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { IoMdSearch } from "react-icons/io";
 import { IoLocationOutline } from "react-icons/io5";
+import { MdOutlineFilterAlt } from "react-icons/md";
 
 const RequestSearchForm = () => {
   const [searchFocused, setSearchFocused] = useState(false);
@@ -8,14 +9,14 @@ const RequestSearchForm = () => {
 
   return (
     <form>
-      <div className="flex flex-col sm:flex-row w-full gap-4">
+      <div className="flex flex-col w-full sm:gap-4 sm:flex-row">
         <div className="relative flex-grow">
           {!searchFocused && (
             <IoMdSearch className="absolute top-1/2 left-3 transform -translate-y-1/2 text-[#CEDBF0] text-[24px]" />
           )}
           <input
             type="text"
-            className="input pl-10 w-full"
+            className="w-full pl-10 input"
             placeholder={
               !searchFocused
                 ? "          Client name or quotation id"
@@ -25,31 +26,39 @@ const RequestSearchForm = () => {
             onBlur={() => setSearchFocused(false)}
           />
         </div>
-        <div className="relative flex-grow">
+        <div className="relative flex-grow mb-4 sm:mb-0">
           {!locationFocused && (
             <IoLocationOutline className="absolute top-1/2 left-3 transform -translate-y-1/2 text-[#CEDBF0] text-[24px]" />
           )}
           <input
             type="text"
-            className="input pl-10 w-full"
+            className="w-full pl-10 input "
             placeholder={!locationFocused ? "          Location" : "Location"}
             onFocus={() => setLocationFocused(true)}
             onBlur={() => setLocationFocused(false)}
           />
         </div>
-        <div>
+        <div className="flex items-center justify-between">
           <button
             type="submit"
-            className="btn-fill py-[8px] px-[24px] text-[14px] w-[143px]"
+            className="btn-fill py-[8px] px-[24px] text-[14px] w-[160px] sm:w-[112px] md:w-[143px]"
           >
             Search
           </button>
+          <div className="flex gap-3 sm:hidden">
+            <button className="flex items-center gap-1 text-[#545A5F] text-[14px] bg-[#FEF7F0] px-[8px] py-[6px]">
+              <MdOutlineFilterAlt /> <p className="text-[12px]">Filters</p>
+            </button>
+            <button type="button" className="text-[#E45416] text-[14px]">
+              Clear
+            </button>
+          </div>
         </div>
       </div>
-      <div className="mt-2 flex gap-3">
+      <div className="flex-wrap hidden gap-3 mt-2 sm:flex">
         <select
           id="countries"
-          className="border border-[#CEDBF0] text-gray-900 text-sm rounded-lg focus:ring-[#EE723C] focus:border-[#EE723C] focus:outline-[#EE723C] focus:outline block p-2.5 "
+          className="border border-[#CEDBF0] text-[#545A5F] text-[14px] text-sm rounded-lg focus:ring-[#EE723C] focus:border-[#EE723C] focus:outline-[#EE723C] focus:outline block p-2.5 "
         >
           <option selected>Service</option>
           <option value="US">United States</option>
@@ -59,7 +68,7 @@ const RequestSearchForm = () => {
         </select>
         <select
           id="countries"
-          className="border border-[#CEDBF0] text-gray-900 text-sm rounded-lg focus:ring-[#EE723C] focus:border-[#EE723C] focus:outline-[#EE723C] focus:outline block p-2.5 "
+          className="border border-[#CEDBF0] text-[#545A5F] text-[14px] text-sm rounded-lg focus:ring-[#EE723C] focus:border-[#EE723C] focus:outline-[#EE723C] focus:outline block p-2.5 "
         >
           <option selected>Roof Type</option>
           <option value="US">United States</option>
@@ -69,7 +78,7 @@ const RequestSearchForm = () => {
         </select>
         <select
           id="countries"
-          className="border border-[#CEDBF0] text-gray-900 text-sm rounded-lg focus:ring-[#EE723C] focus:border-[#EE723C] focus:outline-[#EE723C] focus:outline block p-2.5 "
+          className="border border-[#CEDBF0] text-[#545A5F] text-[14px] text-sm rounded-lg focus:ring-[#EE723C] focus:border-[#EE723C] focus:outline-[#EE723C] focus:outline block p-2.5 "
         >
           <option selected>Wattage</option>
           <option value="US">United States</option>
@@ -79,7 +88,7 @@ const RequestSearchForm = () => {
         </select>
         <select
           id="countries"
-          className="border border-[#CEDBF0] text-gray-900 text-sm rounded-lg focus:ring-[#EE723C] focus:border-[#EE723C] focus:outline-[#EE723C] focus:outline block p-2.5 "
+          className="border border-[#CEDBF0] text-[#545A5F] text-[14px] text-sm rounded-lg focus:ring-[#EE723C] focus:border-[#EE723C] focus:outline-[#EE723C] focus:outline block p-2.5 "
         >
           <option selected>Storeys</option>
           <option value="US">United States</option>
@@ -87,7 +96,7 @@ const RequestSearchForm = () => {
           <option value="FR">France</option>
           <option value="DE">Germany</option>
         </select>
-        <button type="button" className="btn-bg-none">
+        <button type="button" className="text-[#E45416] text-[14px]">
           Clear
         </button>
       </div>
--- client/src/pages/request/RequestPage.jsx
+++ client/src/pages/request/RequestPage.jsx
@@ -2,21 +2,21 @@ import RequestSearchForm from "../../components/request/RequestSearchForm";
 
 const RequestPage = () => {
   return (
-    <div className="min-h-screen ">
-      <div className="bg-red-400 h-[272px]"></div>
-      <div className="flex flex-col w-full ">
-        <div className="flex flex-col mx-auto w-[90%] md:w-[90%] lg:w-[80%] gap-8 -mt-[33vh] mb-[13vh]">
-          <div className=" gap-5 mt-[50px] text-left">
-            <h1 className="text-[32px] sm:text-[36px] font-[500] mb-2 text-[#141920]">
+    <div className="min-h-screen bg-gray-100">
+      <div className="bg-[#FFF8F1] h-[300px] sm:h-[272px]"></div>
+      <div className="flex flex-col items-center w-full">
+        <div className="flex flex-col w-full max-w-screen-lg gap-8 -mt-[252px] sm:-mt-[230px] p-4">
+          <div className="text-left">
+            <h1 className="mb-2 text-[32px] sm:text-[36px] font-semibold text-gray-800 sm:text-5xl">
               Quotation Requests
             </h1>
-            <p className="text-[16px] text-[#545A5F] w-full sm:w-[60%] md:w-[50%] lg:w-[45%] xl:w-[40%] 2xl:w-[30%]">
+            <p className="sm:w-[55%] md:w-[40%] lg:w-[38%]  sm:text-[16px] text-gray-600">
               Discover quotations that pique your interest and submit your
               offers.
             </p>
           </div>
           <div>
-            <div className="border-[1px] p-6 rounded-[16px] bg-[#ffffff]">
+            <div className="p-6 bg-white border border-gray-300 rounded-lg">
               <RequestSearchForm />
             </div>
           </div>
@@ -25,4 +25,5 @@ const RequestPage = () => {
     </div>
   );
 };
+
 export default RequestPage;
