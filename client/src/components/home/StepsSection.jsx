59148a067ec901de028863d53048462e78942417
YasithaRDK <yasithadilshanrajapaksha@gmail.com>
2024-05-20 12:04:46 +0530

    Changes in step section UI

--- client/src/components/home/StepsSection.jsx
+++ client/src/components/home/StepsSection.jsx
@@ -3,18 +3,19 @@ import HeadingTwo from "../HeadingTwo";
 const StepsSection = () => {
   return (
     <div className="relative flex flex-col md:flex-row justify-center items-start text-[#ffffff] py-32 gap-16 md:gap-32 max-w-screen-xl mx-auto p-8">
-      <div className="">
-        <HeadingTwo text="How it works?" isColorChange={true} />
-        <h1 className="hidden sm:block text-[48px] leading-[72px]">
+      <div className="md:w-1/2">
+        <div className="text-center sm:text-left">
+          <HeadingTwo text="How it works?" isColorChange={true} />
+        </div>
+        <h1 className="text-[48px] leading-[72px]  text-center sm:text-left sm:w-[380px]">
           Simple Steps to Solar Success
         </h1>
-        <h1 className="sm:hidden text-[48px] leading-[72px]">
-          Simple Steps to Solar Success
-        </h1>
-        <p>Experience hassle-free solar procurement with SolarMarket.</p>
+        <p className="text-[20px] text-[#929292]  text-center sm:w-[350px] md:w-[350px] mx-auto sm:text-left sm:mx-0">
+          Experience hassle-free solar procurement with SolarMarket.
+        </p>
       </div>
-      <div className="w-[80%] md:w-auto">
-        <ul className="flex flex-col gap-5">
+      <div className="md:w-1/2">
+        <ul className="flex flex-col w-5/6 gap-5 sm:w-4/5 md:w-auto">
           <li className="flex items-start gap-4">
             <h1 className="flex text-[110px] -mt-[46px] text-[#E45416] font-bold font-calibri">
               1
